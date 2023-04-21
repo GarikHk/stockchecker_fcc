3efe9394e4981331adfd91a36d6f62faf294bd5c
Garik H <garik.hakobyan.2004@gmail.com>
2023-04-21 17:10:48 +0400

    feat: added get request for one stock

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,37 @@
 'use strict';
+const fetch = require('node-fetch');
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async (req, res) => {
+      const stock = req.query.stock;
+
+      const getPrice = async (symbol) => {
+        let url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote`;
+        let price;
+
+        await fetch(url)
+          .then(res => res.json())
+          .then(item => {
+            price = item.latestPrice;
+          });
+
+        return {
+          symbol: symbol,
+          price: price,
+        };
+      };
+
+      if (Array.isArray(stock)) {
+        if (stock.length === 2) {
+          console.log("Two Elements");
+        } else console.log("error");
+      };
+
+      res.status(200).json({
+        stockData: await getPrice(stock),
+      });
     });
-    
+
 };
